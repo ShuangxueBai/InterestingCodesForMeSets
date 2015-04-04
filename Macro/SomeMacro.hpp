9874633e13d5401bb5c64b0dd4726ac4e97c4a32
Shuangxue Bai <bshx_1986@163.com>
2015-04-05 03:45:51 +0800

    add SET_AND_GET_PROPERTY

--- Macro/SomeMacro.hpp
+++ Macro/SomeMacro.hpp
@@ -4,7 +4,9 @@
 #define SET_PROPERTY(_type, _methodName, _property)  void set##_methodName## ( const _type##& prop) { (_property) = prop; }
 #define GET_PROPERTY(_type, _methodName, _property) _type get##_methodName## () const { return (_property); }
 
-
+#define SET_AND_GET_PROPERTY(_type, _methodName, _property) \
+	SET_PROPERTY(_type, _methodName, _property); \
+	GET_PROPERTY(_type, _methodName, _property); \
 
 #endif
 
